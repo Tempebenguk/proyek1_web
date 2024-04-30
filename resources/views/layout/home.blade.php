daa21e9797350b6809ad1db2abcd0596066a28bb
Tempebenguk <erricksb592@gmail.com>
2024-04-30 08:59:17 +0700

    heheheheheheh

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -13,6 +13,13 @@
       align-items: center;
       opacity: 0.4;
       margin: auto;
+    }
+
+    .image{
+      margin-top: 10px;
+      width: auto;
+      height: 100px;
+      opacity: 1;
 
     }
 
@@ -23,10 +30,37 @@
       position: absolute;
     }
 
+    .column{
+      float: left;
+      width: 50%;
+    }
+
     .card{
       margin-top: 25px;
-      width: 200px;
+      margin-left: 5px;
       height: auto;
+      float: left;
+      border-radius: 9px;
+      border: 3px solid #BA7237;
+    }
+
+    .kategori{
+      margin-top: 15px; 
+      margin-left: 5px; 
+      width: 100px; 
+      font-size: 12px; 
+      border-radius: 50px;
+      font-weight: bold;
+      background-color: #0000;
+      border-color: #00E432;
+    }
+
+    .btn{
+      background-color: #00E432;
+      width: 135px;
+      border-color: #00E432;
+      border-radius: 3px;
+      font-weight: bold;
     }
 
     .navbar-custom {
@@ -139,27 +173,74 @@
   <div class="logo">
     <img src="{{ asset('starling.png')}}" width="70%" height="70%">
   </div>
-  <div class="container text-center">
+  <div class="container">
   <div class="row">
-    <div class="col">
-    <div class="card">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+  </div>
+    <div class="column"> <!-- Kolom kiri -->
+      <div class="card" style="margin-top: 25px;">
+      <img src="image_1.png" class="image">
+        <div class="card-body">
+          <h5 class="card-title" style="font-size: 10pt; color: #BA7237; ">Kopi Good Day</h5>
+          <p class="card-text" style="font-size: 15pt; font-weight: bold; color: #7C2B18;">Rp 5.000</p>
+          <a href="#" class="btn btn-primary">TAMBAH</a>
+        </div>
+      </div>
     </div>
-    <div class="col">
-      Column
+    <div class="column"> <!-- Kolom kanan -->
+      <div class="card" style="margin-top: 25px;">
+      <img src="image_3.png" class="image" >
+        <div class="card-body">
+          <h5 class="card-title" style="font-size: 10pt; color: #BA7237;">Pop Mie Besar</h5>
+          <p class="card-text" style="font-size: 15pt; font-weight: bold; color: #7C2B18;">Rp 7.000</p>
+          <a href="#" class="btn btn-primary">TAMBAH</a>
+        </div>
+      </div>
+    </div>
+    <div class="column"> <!-- Kolom kiri -->
+      <div class="card">
+      <img src="image_4.png" class="image" style="width: 120px; height: 100px;">
+        <div class="card-body">
+          <h5 class="card-title" style="font-size: 10pt; color: #BA7237; ">Teh Panas/Dingin</h5>
+          <p class="card-text" style="font-size: 15pt; font-weight: bold; color: #7C2B18;">Rp 4.000</p>
+          <a href="#" class="btn btn-primary">TAMBAH</a>
+        </div>
+      </div>
+    </div>
+    <div class="column"> <!-- Kolom kanan -->
+      <div class="card">
+      <img src="image_2.png" class="image" style="width: 120px; ">
+        <div class="card-body">
+          <h5 class="card-title" style="font-size: 10pt; color: #BA7237;">Chocolatos Drink</h5>
+          <p class="card-text" style="font-size: 15pt; font-weight: bold; color: #7C2B18;">Rp 5.000</p>
+          <a href="#" class="btn btn-primary">TAMBAH</a>
+        </div>
+      </div>
+    </div>
+    <div class="column"> <!-- Kolom kiri -->
+      <div class="card">
+      <img src="image_6.png" class="image" style="width: 120px; height: 100px;">
+        <div class="card-body">
+          <h5 class="card-title" style="font-size: 10pt; color: #BA7237; ">Beng Beng Panas/Dingin</h5>
+          <p class="card-text" style="font-size: 15pt; font-weight: bold; color: #7C2B18;">Rp 5.000</p>
+          <a href="#" class="btn btn-primary">TAMBAH</a>
+        </div>
+      </div>
+    </div>
+    <div class="column"> <!-- Kolom kanan -->
+      <div class="card">
+      <img src="image_5.png" class="image" style="width: 120px; ">
+        <div class="card-body">
+          <h5 class="card-title" style="font-size: 10pt; color: #BA7237;">Kopi Hitam</h5>
+          <p class="card-text" style="font-size: 15pt; font-weight: bold; color: #7C2B18;">Rp 4.000</p>
+          <a href="#" class="btn btn-primary">TAMBAH</a>
+        </div>
+      </div>
     </div>
   </div>
 </div>
-  <!-- <div class="card">
-
-  </div> -->
 </div>
-
   </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq"></script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq"></script>
+  </body>
 </html>
